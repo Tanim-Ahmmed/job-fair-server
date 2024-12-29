8de42355fef4bf662e00df579f1d9b44ac6cff18
Tanim-Ahmmed <tanimahmmed234@gmail.com>
2024-12-29 11:47:50 +0600

    jwt toekn veification done

--- index.js
+++ index.js
@@ -1,18 +1,34 @@
-const express = require('express');
-const cors = require('cors');
-const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const express = require("express");
+const cors = require("cors");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const app = express();
-require('dotenv').config()
-const port = process.env.PORT || 5000 ;
+const jwt = require("jsonwebtoken");
+const cookieParser = require("cookie-parser");
+require("dotenv").config();
+const port = process.env.PORT || 5000;
 
-app.use(cors());
+app.use(cors({
+  origin: ["http://localhost:5173"],
+  credentials: true,
+}));
 app.use(express.json());
+app.use(cookieParser());
+ 
+const verifyToken = (req, res , next) =>{
+  // console.log('inside verify token',  req.cookies);
+  const token = req.cookies?.token;
 
-
-
-
-
-
+  if(!token){
+    return res.status(401).send( {message: "Access Denied"});
+  } 
+  jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
+    if(err){
+      return res.status(401).send({message: 'unauthorized'});
+    }
+    req.user = decoded;
+    next();
+  }) 
+}
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.fgufh.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
@@ -24,7 +40,7 @@ const client = new MongoClient(uri, {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
 
 async function run() {
@@ -33,43 +49,59 @@ async function run() {
     await client.connect();
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
 
+    const jobsCollection = client.db("job-fair").collection("jobs");
+    const jobApplicationCollection = client
+      .db("job-fair")
+      .collection("job_applications");
 
+    //jwt
+    app.post("/jwt", async (req, res) => {
+      const user = req.body;
+      const token = jwt.sign(user, process.env.JWT_SECRET, {expiresIn: "1h"});
 
+      res.cookie("token", token,{
+        httpOnly:true,
+        secure: false,
+        // sameSite: "strict",
+      } )
+      .send({success: true})
+      
+    });
 
-
-    const jobsCollection = client.db("job-fair").collection('jobs');
-    const jobApplicationCollection = client.db("job-fair").collection('job_applications')
-
-
-    app.get('/jobs/:id', async(req, res) =>{
+    app.get("/jobs/:id", async (req, res) => {
       const id = req.params.id;
-      const query = {_id: new ObjectId(id)}
+      const query = { _id: new ObjectId(id) };
       const result = await jobsCollection.findOne(query);
       res.send(result);
-    })
+    });
 
-    app.get('/jobs', async(req, res) =>{
+    app.get("/jobs", async (req, res) => {
       const cursor = jobsCollection.find();
       const result = await cursor.toArray();
       res.send(result);
-    })
+    });
 
-    app.get('/job-application', async (req, res) =>{
+    app.get("/job-application", verifyToken, async (req, res) => {
       const email = req.query.email;
-      const query = {applicant_email: email}
+      const query = { applicant_email: email };
+
+      if(req.user.email !== email){
+        return res.status(403).send({message: "forbidden"});
+      }
+    
       const result = await jobApplicationCollection.find(query).toArray();
       res.send(result);
-    })
+    });
 
-
-    app.post('/job-applications', async (req, res) =>{
+    app.post("/job-applications", async (req, res) => {
       const application = req.body;
-    const result = await  jobApplicationCollection.insertOne(application);
-    res.send(result);
-    })
-
+      const result = await jobApplicationCollection.insertOne(application);
+      res.send(result);
+    });
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
@@ -77,11 +109,10 @@ async function run() {
 }
 run().catch(console.dir);
 
+app.get("/", (req, res) => {
+  res.send("job is falling from the sky");
+});
 
-app.get('/',(req, res) =>{
-    res.send('job is falling from the sky')
-})
-
-app.listen(port, () =>{
-    console.log(`job is waiting at: ${port}`)
-})
+app.listen(port, () => {
+  console.log(`job is waiting at: ${port}`);
+});
